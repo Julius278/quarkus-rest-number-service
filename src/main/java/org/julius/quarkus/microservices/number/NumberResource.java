b754a970da4419a663587fe43d2a26b42adea6aa
julius278 <julius.lauterbach@t-online.de>
2023-04-25 12:32:36 +0200

    some OpenAPI code comments to NumberResource

--- src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
+++ src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
@@ -1,5 +1,8 @@
 package org.julius.quarkus.microservices.number;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,11 +11,14 @@ import java.time.Instant;
 import java.util.Random;
 
 @Path("/api/numbers")
+@Tag(name = "Number REST Endpoint")
 public class NumberResource {
 
     @GET
+    @Path("/random")
     @Produces(MediaType.APPLICATION_JSON)
-    public IsbnNumbers getIsbnNumbers() {
+    @Operation(summary = "Generates random ISBN numbers for books", description = "some more description")
+    public IsbnNumbers getRandomIsbnNumbers() {
         IsbnNumbers isbnNumbers = new IsbnNumbers();
         isbnNumbers.setIsbn10("10-" + new Random().nextInt(1_000_000));
         isbnNumbers.setIsbn13("13-" + new Random().nextInt(100_000_000));
