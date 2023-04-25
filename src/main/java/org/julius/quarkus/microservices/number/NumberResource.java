00e08b4cff5f4bcaee575128825be5835ffbc6f9
julius278 <julius.lauterbach@t-online.de>
2023-04-25 14:07:42 +0200

    added logger to NumberResource

--- src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
+++ src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
@@ -2,7 +2,9 @@ package org.julius.quarkus.microservices.number;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,6 +16,9 @@ import java.util.Random;
 @Tag(name = "Number REST Endpoint")
 public class NumberResource {
 
+    @Inject
+    private Logger logger;
+
     @GET
     @Path("/random")
     @Produces(MediaType.APPLICATION_JSON)
@@ -23,6 +28,9 @@ public class NumberResource {
         isbnNumbers.setIsbn10("10-" + new Random().nextInt(1_000_000));
         isbnNumbers.setIsbn13("13-" + new Random().nextInt(100_000_000));
         isbnNumbers.setGenerationTime(Instant.now());
+
+        logger.info("Endpoint getRandomIsbnNumbers generated: " + isbnNumbers.toString());
+
         return isbnNumbers;
     }
 }
