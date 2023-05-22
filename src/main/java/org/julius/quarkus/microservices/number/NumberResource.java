51de8685cf9e033da28df1d91b5911ce86d51c48
julius278 <julius.lauterbach@t-online.de>
2023-05-22 14:22:25 +0200

    new POST method for testing purpose

--- src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
+++ src/main/java/org/julius/quarkus/microservices/number/NumberResource.java
@@ -6,15 +6,16 @@ import org.jboss.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.time.Instant;
 import java.util.Random;
 
 @Path("/api/numbers")
+@RequestScoped
 @Tag(name = "Number REST Endpoint")
+@Produces(MediaType.APPLICATION_JSON)
 public class NumberResource {
 
     @Inject
@@ -22,8 +23,6 @@ public class NumberResource {
 
     @GET
     @Path("/random")
-    @RequestScoped
-    @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Generates random ISBN numbers for books", description = "some more description about ISBN10 and ISBN13 numbers")
     public IsbnNumbers getRandomIsbnNumbers() {
         IsbnNumbers isbnNumbers = new IsbnNumbers();
@@ -34,5 +33,16 @@ public class NumberResource {
         logger.info("Endpoint getRandomIsbnNumbers generated: " + isbnNumbers.toString());
 
         return isbnNumbers;
+    }
+
+    @POST
+    @Operation(summary = "just for testing purpose", description = "just for testing purpose")
+    public Response saveRandomIsbnTenNumber(@FormParam("isbn10") String isbn10) {
+        IsbnNumbers isbnNumbers = new IsbnNumbers();
+        isbnNumbers.setIsbn10(isbn10);
+
+        isbnNumbers.setIsbn13("nn");
+
+        return Response.accepted(isbnNumbers).build();
     }
 }
